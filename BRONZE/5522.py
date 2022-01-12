761569697fb621243af3f843de15ad146265b156
Yuhee Kim <79076150+hapen7535@users.noreply.github.com>
2022-01-12 09:54:24 +0900

    5522

---
+++ BRONZE/5522.py
@@ -0,0 +1,10 @@
+ans = 0
+exec("ans += int(input());"*5)
+print(ans)
+
+"""arr = [0 for i in range(5)]
+    ans = 0
+    for i in range(len(arr)):
+        arr[i] = int(input())
+        ans += arr[i]
+    print(ans)"""
