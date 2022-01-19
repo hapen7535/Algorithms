0cc2b92f0f276d7e69f7d059a8baecd4fce7c4b7
Yuhee Kim <79076150+hapen7535@users.noreply.github.com>
2022-01-19 22:29:11 +0900

    2752

---
+++ BRONZE/2752.py
@@ -0,0 +1,21 @@
+a,b,c = map(int, input().split())
+temp = 0
+
+if a>b:
+    temp = a
+    a = b
+    b = temp
+
+if b>c:
+    temp = b
+    b = c
+    c = temp
+
+if a>b:
+    temp = a
+    a = b
+    b = temp
+
+print(a,b,c)
+
+
