e39bfecbf54360beff252f87b85f2ed36bea14da
Yuhee Kim <79076150+hapen7535@users.noreply.github.com>
2022-01-27 19:45:32 +0900

    1010

--- SILVER/1010.py
+++ SILVER/1010.py
@@ -4,8 +4,8 @@ input = sys.stdin.readline
 n = int(input())
 
 #인터프리터 언어이므로 함수는 위에 선언
-def fact(a): #재귀함수를 이용할 경우 캐쉬 메모리를 많이 쓰게되어 시간 초과가 일어날 수 있다.
-    num = 1
+def fact(a): #재귀함수를 이용할 경우 메모리 부족으로 시간 초과가 일어날 수 있다.
+    num = 1 #재귀함수를 쓰더라도 메모이제이션을 쓰면 효율적인 코드가 될 수 있다.
     for i in range(1, a+1):
         num *= i
     return num
