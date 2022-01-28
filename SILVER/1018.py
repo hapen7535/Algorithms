7e03f8587cd1431a359c14d683086350ae597f90
Yuhee Kim <79076150+hapen7535@users.noreply.github.com>
2022-01-28 23:59:15 +0900

    1018

---
+++ SILVER/1018.py
@@ -0,0 +1,27 @@
+#완전 탐색 이용
+N,M = map(int, input().split())
+orig = []
+count = []
+
+for _ in range(N):
+    orig.append(input())
+
+for a in range(N-7):
+    for b in range(M-7):
+        idxa = 0
+        idxb = 0
+        for i in range(a, a+8):
+            for j in range(b, b+8):
+                if (i + j) % 2 == 0: #인덱스가 짝수일 경우
+                    if orig[i][j] != 'W':
+                        idxa += 1
+                    if orig[i][j] != 'B':
+                        idxb += 1
+                else: #인덱스 홀수
+                    if orig[i][j] != 'B':
+                        idxa += 1
+                    if orig[i][j] != 'W':
+                        idxb += 1
+        count.append(min(idxa, idxb))
+
+print(min(count))
