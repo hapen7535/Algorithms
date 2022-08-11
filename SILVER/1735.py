eb1326fb0bd7483fc43ec50fa10175b3ad33286b
Yuhee Kim <dbgml4509@naver.com>
2022-08-12 00:42:01 +0900

    실버 3 분수 합

--- SILVER/1735.py
+++ SILVER/1735.py
@@ -5,15 +5,15 @@ def gcd(a,b):
   while b != 0:
     a = a % b
     a,b = b,a
-  return b
+  return a
 
 A1,B1 = map(int, input().split())
 A2,B2 = map(int, input().split())
 
 gcdValue = gcd(B1,B2)
+num = A1 * (B2//gcdValue) + A2 * (B1//gcdValue)
 demon = B1 * B2 // gcdValue
-mole = A1 * (B2//gcdValue) + A2 * (B1//gcdValue)
 
-gcdValue2 = gcd(demon,mole)
-
-
+#기약분수
+gcdValue2 = gcd(demon,num)
+print(num // gcdValue2, demon //gcdValue2)
