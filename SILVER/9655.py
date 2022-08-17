09885e54ec44790e9245e065836cf7c6ff649ce6
Yuhee Kim <dbgml4509@naver.com>
2022-08-18 00:45:11 +0900

    실버 5 돌 게임

---
+++ SILVER/9655.py
@@ -0,0 +1,21 @@
+#dp
+
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+dp = [-1]*1001 #입력 1000이 최대
+dp[1] = 1
+dp[2] = 0
+dp[3] = 1
+
+for i in range(4, N + 1):
+  if dp[i - 1] or dp[i - 3]: #홀수일 경우 
+    dp[i] = 0
+  else:
+    dp[i] = 1
+
+if dp[N] == 0:
+  print("CY")
+else:
+  print("SK")
