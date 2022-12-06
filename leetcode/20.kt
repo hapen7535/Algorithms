d3136e6200f6673762106f8c2791d328724c82b3
Yuhee Kim <dbgml4509@naver.com>
2022-12-06 22:04:44 +0900

    refactor : 상수 변수로 치환

--- leetcode/20.kt
+++ leetcode/20.kt
@@ -1,14 +1,9 @@
 class Solution {
     fun isValid(s: String): Boolean {
         if (s.length % 2 == 1) return false
-        val closeBracket = hashMapOf(
-            '(' to ')',
-            '[' to ']',
-            '{' to '}'
-        )
 
-        val validKeys = closeBracket.keys;
-        var validStack = ArrayDeque<Char>()
+        val validKeys = CLOSE_BRACKET.keys;
+        val validStack = ArrayDeque<Char>()
 
         for (element in s) {
             if (validKeys.contains(element)) {
@@ -16,10 +11,19 @@ class Solution {
             } else {
                 if (validStack.isEmpty()) return false;
                 val charToCheck = validStack.removeLast()
-                if (closeBracket.get(charToCheck) != element) return false
+                if (CLOSE_BRACKET.get(charToCheck) != element) return false
             }
         }
 
         return validStack.isEmpty()
     }
+
+    companion object {
+        private val CLOSE_BRACKET: HashMap<Char, Char> = hashMapOf(
+            '(' to ')',
+            '[' to ']',
+            '{' to '}'
+        )
+    }
+
 }
