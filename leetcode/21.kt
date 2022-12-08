37b93a536c223e9f512148a83807ace021bc8bc6
Yuhee Kim <dbgml4509@naver.com>
2022-12-08 14:40:21 +0900

    refactor : 상수 변수로 치환

--- leetcode/21.kt
+++ leetcode/21.kt
@@ -1,10 +1,16 @@
 class Solution {
-  fun mergeTwoLists(list1: ListNode?, list2: ListNode?): ListNode? {
+    fun mergeTwoLists(list1: ListNode?, list2: ListNode?): ListNode? {
         if (list1 == null) return list2
         if (list2 == null) return list1
         return when (list1.`val`.compareTo(list2.`val`)) {
-            0, 1 -> list2.apply { next = mergeTwoLists(list1, list2.next) }
+            EVEN_NUMBER, BEAT_NUMBER -> list2.apply { next = mergeTwoLists(list1, list2.next) }
             else -> list1.apply { next = mergeTwoLists(list2, list1.next) }
         }
     }
+
+    companion object{
+        const val EVEN_NUMBER = 0
+        const val BEAT_NUMBER = 1
+    }
+
 }
