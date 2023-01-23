5608a690b8c12c4665f9bf63d595b10a6ac06c01
Yuhee Kim <dbgml4509@naver.com>
2023-01-24 00:02:21 +0900

    refactor : 상수 변수로 변경

--- leetcode/412.kt
+++ leetcode/412.kt
@@ -2,19 +2,23 @@ class Solution {
     fun fizzBuzz(n: Int): List<String> {
         val answer = ArrayList<String>()
         for(i in 1..n){
-            if(i % 3 == 0 && i % 5 == 0){
+            if(i % FIZZ_NUMBER == 0 && i % BUZZ_NUMBER == 0){
                 answer.add("FizzBuzz")
             }
-            else if(i % 3 == 0){
+            else if(i % FIZZ_NUMBER == 0){
                 answer.add("Fizz")
             }
-            else if(i % 5 == 0){
+            else if(i % BUZZ_NUMBER == 0){
                 answer.add("Buzz")
             }
             else{
-                answer.add("$i") 
+                answer.add(i.toString()) //"$i"
             }
             }
-         return list
+         return answer
         }
+    companion object{
+        const val FIZZ_NUMBER = 3
+        const val BUZZ_NUMBER = 5
+    }
 } 
