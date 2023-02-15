912abf4221259654d0821f417236e9c0224f4f70
Yuhee Kim <dbgml4509@naver.com>
2023-02-15 22:33:38 +0900

    level1 : 숫자 짝꿍

---
+++ programmers/PairNumber.kt
@@ -0,0 +1,30 @@
+class Solution {
+    fun solution(X: String, Y: String): String {
+        var answer = StringBuilder()
+
+        val result = mutableListOf<Int>()
+        val xList = X.toCharArray().map { it.digitToInt() }.sorted()
+        val yList = Y.toCharArray().map { it.digitToInt() }.sorted()
+
+        var index = 0
+        
+        xList.forEach {
+            for(i in index until yList.size) {
+                if(yList[i] == it) {
+                    result.add(it)
+                    index = i + 1
+                    break
+                }
+            }
+        }
+
+        result.sortedByDescending{ it }.forEach {
+            answer.append(it)
+        }
+
+        if(answer.isEmpty()) return "-1"
+        else if(answer[0] == '0') return "0"
+
+        return answer.toString()
+    }
+}
