46cdc9ca1a2a176eb9caabb0808a220f9d562c8a
Yuhee Kim <dbgml4509@naver.com>
2023-02-26 00:02:06 +0900

    Update BiggestNumber.kt

--- programmers/BiggestNumber.kt
+++ programmers/BiggestNumber.kt
@@ -37,30 +37,5 @@ class Solution {
         return answer
     }
     
-    //다른 풀이 스택 이용
-    fun solution3(number: String, k: Int): String {
-        var answer = ""
-        var kCnt = k
-        val numberStack : Stack<Char> = Stack()
-        var numArray = CharArray(number.length-k)
-
-
-        number.forEach {
-            while (!numberStack.isEmpty()&&numberStack.peek()<it&&kCnt!=0){
-                numberStack.pop()
-                kCnt--
-            }
-            numberStack.push(it)
-        }
-
-        for (i in 0 until kCnt){
-            numberStack.pop()
-        }
-
-        numberStack.forEachIndexed { index, c ->
-            numArray[index] = c
-        }
-
-        return String(numArray)
-    }
+  
 }
