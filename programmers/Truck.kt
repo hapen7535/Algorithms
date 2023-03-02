5e66b7d4e65eb142a206111de95262f538c07209
Yuhee Kim <dbgml4509@naver.com>
2023-03-02 21:11:50 +0900

    level2 : 다리를 지나는 트럭 솔루션 추가

--- programmers/Truck.kt
+++ programmers/Truck.kt
@@ -16,6 +16,9 @@ class Solution {
             trucksInBridge.add(0)
         }
         
+//         val trucksInBridge: Queue<Int> = LinkedList(List(bridge_length){0})
+//         val truck: Queue<Int> = LinkedList(truck_weights.toList())
+        
         while(!trucksInBridge.isEmpty()){
             if(trucksInBridge.peek() > 0) numberOfBridgeWeight += trucksInBridge.peek()
             
@@ -34,4 +37,24 @@ class Solution {
         
         return answer
     }
+    
+    fun solution2(bridge_length: Int, weight: Int, truck_weights: IntArray): Int {
+        var answer = 0
+        val bridgeQueue: Queue<Int> = LinkedList(List(bridge_length){0})
+        val waitingQueue: Queue<Int> = LinkedList(truck_weights.toList())
+        
+        while(bridgeQueue.isNotEmpty()) {
+            answer++
+            bridgeQueue.poll()
+            if(waitingQueue.isNotEmpty()){
+                if(bridgeQueue.sum() + waitingQueue.peek() <= weight){
+                    bridgeQueue.add(waitingQueue.poll())
+                } else{
+                    bridgeQueue.add(0)
+                }
+            }
+        }
+        return answer
+    }
+    
 }
