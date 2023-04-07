f120382c02c6411dff9dbf2fe24019f3ea9ab810
Yuhee Kim <dbgml4509@naver.com>
2023-04-07 23:56:54 +0900

    Update TwoDimension.kt

--- programmers/TwoDimension.kt
+++ programmers/TwoDimension.kt
@@ -1,15 +1,13 @@
 class Solution {
-    fun solution(num_list: IntArray, n: Int): Array<IntArray> {
-        var answer: Array<IntArray> = arrayOf<IntArray>()
-        
-        for(i in 0 until num_list.size){
-            val arr = ArrayList<Int>()
-            for(j in 0 until n){
-                arr.add(num_list[i])
+    fun solution(numList: IntArray, n: Int): Array<IntArray> {
+        var count = 0
+        val answer = Array(numList.size / n) { IntArray(n) }
+        for (i in 0 until (numList.size / n)) {
+            for (j in 0 until n) {
+                answer[i][j] = numList[count]
+                count++
             }
-            answer.add(arr[i])
         }
-        
         return answer
     }
 }
