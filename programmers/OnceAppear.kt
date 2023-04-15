86457b8dca170607cd4ade9613e9ef0e595b5e7f
Yuhee Kim <dbgml4509@naver.com>
2023-04-15 23:59:00 +0900

    Update OnceAppear.kt

--- programmers/OnceAppear.kt
+++ programmers/OnceAppear.kt
@@ -2,5 +2,15 @@ class Solution{
      fun solution(s: String) = s.toSet()
         .filter { uniqueChar ->
             s.count { it == uniqueChar } == 1
-        }.sorted().joinToString("")   
+        }.sorted().joinToString("")  
+        
+     fun solution2(s: String): String {
+        return s.split("".toRegex())
+            .groupBy { it }
+            .entries
+            .filter { (_, value) -> value.size <= 1 }
+            .map { (key, _) -> key }
+            .sorted()
+            .joinToString("")
+    }
 }
